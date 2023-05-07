d1718108d2c216bb87a7e7987d2386b09578252c
Junad-Park <qkrwnsgk1624@icloud.com>
2023-05-07 22:27:12 +0900

    ~ section 3

---
+++ hello-spring/src/main/java/likelion/hellospring/domain/Member.java
@@ -0,0 +1,13 @@
+package likelion.hellospring.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Member {
+
+    private Long id;
+    private String name;
+
+}
