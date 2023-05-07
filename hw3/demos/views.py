dc872dd5ed8a5659d581ae4813f47b3b97e9a8d3
마재성 <majesoung@naver.com>
2023-05-07 21:51:40 +0900

    [feat]: 계산기 기능 추가

--- hw3/demos/views.py
+++ hw3/demos/views.py
@@ -4,6 +4,28 @@ from django.http import HttpResponse
 
 def Calc(request):
     # return HttpResponse("계산기 기능 구현 시작 이게 맞나")
-    return render(request, 'calc.html')
+    
+    #데이터 받아오기
+    
+    num1 = request.GET.get('num1')
+    num2 = request.GET.get('num2')
+    operators = request.GET.get('operators')
+    
+    
+    # 기능 
+    if operators == '+':
+        result = int(num1) + int(num2)
+    elif operators == '-':
+        result = int(num1) - int(num2)
+    elif operators == '*':
+        result = int(num1) * int(num2)
+    elif operators == '/':
+        result = int(num1) / int(num2)
+    else:
+        result = 0 
+
+        
+    
+    return render(request, 'calc.html', {"result": result})
 
 
