16efd2429c638a573e810d9a407441f9ec137da6
마재성 <majesoung@naver.com>
2023-05-07 20:42:50 +0900

    [feat]: template 사용을 위한 view 기능과 html 추가

--- hw3/demos/views.py
+++ hw3/demos/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def Calc(request):
+    # return HttpResponse("계산기 기능 구현 시작 이게 맞나")
+    return render(request, 'calc.html')
+
+
