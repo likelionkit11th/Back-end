8ebdb7748c5fcf37132642d465388d138100c9ea
Seol_JY <wlsdud5654@gmail.com>
2023-05-09 00:18:39 +0900

    init: hw3

---
+++ HW3SpringBoard/src/main/java/com/seol/HW3SpringBoard/Hw3SpringBoardApplication.java
@@ -0,0 +1,13 @@
+package com.seol.HW3SpringBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw3SpringBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw3SpringBoardApplication.class, args);
+	}
+
+}
---
+++ HW3SpringBoard/src/test/java/com/seol/HW3SpringBoard/Hw3SpringBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seol.HW3SpringBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw3SpringBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
