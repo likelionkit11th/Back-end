ee2de7c95d1cb53fb56edd1a0e4bbffa083e1d8a
마재성 <majesoung@naver.com>
2023-05-08 23:27:20 +0900

    feat: admin 페이지 커스터마이징

--- liongram/posts/admin.py
+++ liongram/posts/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 from .models import Post, Comment
 # Register your models here.
 
-admin.site.register(Post)
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'image', 'content', 'date', 'view_Count', 'writer')
+    list_filter = ('date',)
+    search_fields = ('id','writer__username')
+    search_help_text = "게시판 번호, 작성자 검색이 가능합니다."
+admin.site.register(Post,PostModelAdmin)
 admin.site.register(Comment)
