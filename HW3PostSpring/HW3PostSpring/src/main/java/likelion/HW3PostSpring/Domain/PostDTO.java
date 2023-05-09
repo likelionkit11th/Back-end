20a1d5128cbb8fcc4065784bcb860f4ef8332cad
eunslog <yeseo235@gmail.com>
2023-05-09 21:42:25 +0900

    homework3

---
+++ HW3PostSpring/HW3PostSpring/src/main/java/likelion/HW3PostSpring/Domain/PostDTO.java
@@ -0,0 +1,11 @@
+package likelion.HW3PostSpring.Domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PostDTO {
+    private String title;
+    private String description;
+}
