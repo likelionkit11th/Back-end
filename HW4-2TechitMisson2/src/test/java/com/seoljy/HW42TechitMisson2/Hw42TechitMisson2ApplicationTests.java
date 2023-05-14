c8ed73ae341baacc7efb5ba69262a5d88517f6e8
Seol_JY <wlsdud5654@gmail.com>
2023-05-15 02:35:42 +0900

    init: hw4-2

---
+++ HW4-2TechitMisson2/src/test/java/com/seoljy/HW42TechitMisson2/Hw42TechitMisson2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seoljy.HW42TechitMisson2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw42TechitMisson2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
