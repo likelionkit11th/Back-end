4ae9408b0091e3b040ec1842739804a9a70b78dd
마재성 <majesoung@naver.com>
2023-05-16 02:40:06 +0900

    chore : date 필드 명 변경 -> create_at

--- liongram/posts/admin.py
+++ liongram/posts/admin.py
@@ -18,11 +18,11 @@ class CommentInline(admin.TabularInline): # 댓글 정렬이 가로로
 #     verbose_name_plural = "댓글" #복수형
     
 class PostModelAdmin(admin.ModelAdmin):
-    list_display = ('id', 'image', 'content', 'date', 'view_Count', 'writer') #보여질 필드 설정
-    list_filter = ('date',) #필터 옵션
+    list_display = ('id', 'image', 'content', 'created_at', 'view_Count', 'writer') #보여질 필드 설정
+    list_filter = ('created_at',) #필터 옵션
     search_fields = ('id','writer__username') #검색 가능 한 필드 설정
     search_help_text = "게시판 번호, 작성자 검색이 가능합니다." # 검색창 밑에 뜰 문장
-    readonly_fields = ('date', ) # 올린 날짜 보이는데 readonly 로
+    readonly_fields = ('created_at', ) # 올린 날짜 보이는데 readonly 로
     inlines = [CommentInline] #게시글에 댓글 보이도록 설정
     actions = ['make_published']
     
--- liongram/posts/models.py
+++ liongram/posts/models.py
@@ -8,13 +8,13 @@ User = get_user_model()
 class Post(models.Model):
     image = models.ImageField(verbose_name='이미지', null = True, blank = True ) # 사용자나 관리자나 field에 대한 이름을 지정
     content = models.TextField(verbose_name='내용')
-    date = models.DateTimeField(verbose_name='작성일', auto_now_add=True) 
+    created_at = models.DateTimeField(verbose_name='작성일', auto_now_add=True) 
     view_Count = models.IntegerField(verbose_name='조회수', default = 0)
     writer = models.ForeignKey(to=User, on_delete=models.CASCADE, null = True, blank = True)
     
 
 class Comment(models.Model):
     content = models.TextField(verbose_name='내용')
-    date = models.DateTimeField(verbose_name='작성일',auto_now_add=True)
+    created_at = models.DateTimeField(verbose_name='작성일',auto_now_add=True)
     post = models.ForeignKey(to='Post', on_delete=models.CASCADE)
     writer = models.ForeignKey(to=User, on_delete=models.CASCADE)
