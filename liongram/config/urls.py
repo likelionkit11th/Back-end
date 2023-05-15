55a3ec2689d8591812a71af51b56203e48bf6092
마재성 <majesoung@naver.com>
2023-05-16 02:40:49 +0900

    feat: django tool bar 세팅

--- liongram/config/urls.py
+++ liongram/config/urls.py
@@ -5,7 +5,6 @@ from django.conf.urls.static import static
 from posts.views import url_view, url_param, function_view, class_view, index
 
 
-
 urlpatterns = [
     path('', index, name='index'),
     path("admin/", admin.site.urls),
@@ -14,6 +13,10 @@ urlpatterns = [
     path('fbv/', function_view),
     path('cbv/', class_view.as_view(), name='cbv'),
     path('posts/',include('posts.urls')),
+    path('__debug__/', include('debug_toolbar.urls')),
+
 ]
 
+
+    
 urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
