4b66f043b4807070b0224da3f64034d4a3644a4b
마재성 <majesoung@naver.com>
2023-05-16 00:26:42 +0900

    feat: create 실습

--- liongram/posts/urls.py
+++ liongram/posts/urls.py
@@ -6,7 +6,7 @@ app_name = 'posts'
 
 urlpatterns = [
     path('',post_list_view, name="post-list"),
-    path('new/', post_create_view),
+    path('create/', post_create_view, name="post-create"),
     path('<int:id>/', post_detail_view),
     path('<int:id>/edit/', post_update_view),
     path('<int:id>/delete/', post_delete_view),
--- liongram/posts/views.py
+++ liongram/posts/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
 
 from django.views.generic.list import ListView
-
+from django.contrib.auth.decorators import login_required
 from .models import Post
 
 
@@ -12,8 +12,21 @@ def index(request):
 def post_list_view(request):
     return render(request, 'posts/post_list.html')
 
+@login_required
 def post_create_view(request):
-    return render(request, 'posts/post_form.html')
+    if request.method == 'GET':
+        return render(request, 'posts/post_form.html')
+    else:
+        image = request.FILES.get('image')
+        content = request.POST.get('content')
+        print(image)
+        print(content)
+        Post.objects.create(
+            image=image,
+            content=content,
+            writer=request.user
+        )
+        return redirect('index')
 
 def post_update_view(request, id):
     return render(request, 'posts/post_form.html')
