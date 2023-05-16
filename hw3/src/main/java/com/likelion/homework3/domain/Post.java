d564a8c8d8f61bc5659254a2cfd35c1298dd57fd
minturtle <minseok0425@kakao.com>
2023-05-16 16:05:26 +0900

    gd zz

--- hw3/src/main/java/com/likelion/homework3/domain/Post.java
+++ hw3/src/main/java/com/likelion/homework3/domain/Post.java
@@ -4,26 +4,24 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @Entity
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Post {
 
-    @Builder
-    public Post(String title, String description) {
-        this.title = title;
-        this.description = description;
-    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String description;
+
+    private LocalDateTime createdAt;
 }
