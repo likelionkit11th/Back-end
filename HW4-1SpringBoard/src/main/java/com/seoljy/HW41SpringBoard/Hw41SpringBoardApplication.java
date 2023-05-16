b5144c2abd532396057d6b560bbe085c00e8b79c
Seol_JY <wlsdud5654@gmail.com>
2023-05-16 10:05:06 +0900

    init: hw4-1

---
+++ HW4-1SpringBoard/src/main/java/com/seoljy/HW41SpringBoard/Hw41SpringBoardApplication.java
@@ -0,0 +1,13 @@
+package com.seoljy.HW41SpringBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw41SpringBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw41SpringBoardApplication.class, args);
+	}
+
+}
