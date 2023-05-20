9f0f4039edcd707d329d9d2b9fb53bf15a084ab3
마재성 <majesoung@naver.com>
2023-05-20 15:28:55 +0900

    feat: Read 구현 / 홈 화면 디자인

--- myblog/config/urls.py
+++ myblog/config/urls.py
@@ -3,7 +3,7 @@ from django.urls import path,include
 from posts.views import index
 
 urlpatterns = [
-    path("",index),
+    path("",index,name='index'),
     path("posts/",include('posts.urls')),
     path("admin/", admin.site.urls),
 ]
--- myblog/posts/views.py
+++ myblog/posts/views.py
@@ -1,14 +1,33 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .models import Post
 # Create your views here.
 
 
 def index(request):
-    return render(request,'index.html')
+    post_list = Post.objects.all().order_by('-created_at')
+    context={
+        'post_list' : post_list,
+    }
+    return render(request,'index.html',context)
 
 def post_create_view(request):
     if request.method =='GET':
         return render(request, 'posts/post_create.html')
     elif request.method =='POST':
-        pass
-
+        post_title = request.POST.get('post-title')
+        post_content = request.POST.get('post-content')
+        Post.objects.create(
+            title=post_title,
+            content=post_content
+        )
+        return redirect('index')
+    
+def post_detail_view(request,id):
+    try:
+        post = Post.objects.get(id=id)
+    except Post.DoesNotExist:
+        redirect ('index')
+    context={
+        'post' : post,
+    }
+    return render(request,'posts/post_detail',context)
