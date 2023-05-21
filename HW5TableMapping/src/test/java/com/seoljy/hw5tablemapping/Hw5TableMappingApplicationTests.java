8304e453e5bf23f86292b9180cd35fe61f157847
Seol_JY <wlsdud5654@gmail.com>
2023-05-21 20:51:54 +0900

    init: hw5 초기화

---
+++ HW5TableMapping/src/test/java/com/seoljy/hw5tablemapping/Hw5TableMappingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seoljy.hw5tablemapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw5TableMappingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
